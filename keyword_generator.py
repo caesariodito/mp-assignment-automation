89fc49ce3d9dfc008122d04edbf636b63fa76b23
Caesario Dito <caesariodito2014@gmail.com>
2023-02-04 15:09:00 +0700

    Adding documentation
    adding simple documentation with autoDocstring python extension

--- keyword_generator.py
+++ keyword_generator.py
@@ -9,22 +9,39 @@ nltk.download('stopwords')
 nltk.download('wordnet')
 
 def read_pdf():
+  """This function will read the input from the source (pdf/image/text)
+
+  Returns:
+      str: text
+  """
   return "China's panda and Australia's koala are two animals that arent predator, pandas eat bamboo and koala's eat eucalyptus leaves. Therefore, they are harmless. They are both different from pythons because pythons are potentialy dangerous considering they can swallow an entire alligator you could conceivably have pythons shacking upto the Potomac"
 
 def preprocess_text(text):
-    # Stopword removal, converting uppercase into lower case, and lemmatization
-    stopwords = nltk.corpus.stopwords.words('english')
-    lemmatizer = WordNetLemmatizer()
+	"""This function will do a preprocessing of the text input
+
+	Args:
+			str: text input to preprocess (unpreprocessed text)
+
+	Returns:
+			str: preprocessed text
+	"""
+	stopwords = nltk.corpus.stopwords.words('english')
+	lemmatizer = WordNetLemmatizer()
+	
+	p_text = re.sub('[^a-zA-Z]', ' ', text)
+	p_text = p_text.lower()
+	p_text = p_text.split()
+	p_text = [lemmatizer.lemmatize(word) for word in p_text if not word in set(stopwords)]
+	p_text = ' '.join(p_text)
     
-    p_text = re.sub('[^a-zA-Z]', ' ', text)
-    p_text = p_text.lower()
-    p_text = p_text.split()
-    p_text = [lemmatizer.lemmatize(word) for word in p_text if not word in set(stopwords)]
-    p_text = ' '.join(p_text)
-    
-    return p_text
+	return p_text
 
 def get_keywords():
+  """This function will be used to get the keywords from the preprocessed text
+
+  Returns:
+      list: top 5 keywords from the text input
+  """
   text = read_pdf() 
   text = preprocess_text(text)
   term_frequencies = Counter(text.split())
