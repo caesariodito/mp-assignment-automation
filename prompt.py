1cb4cb9cc5f93f33e6d09e8f8523d0bd12c0e781
Caesario Dito <caesariodito2014@gmail.com>
2023-02-05 21:37:03 +0700

    Adding prompt module

---
+++ prompt.py
@@ -0,0 +1,8 @@
+DESIGN_PROMPT = """
+Act as an expert in the topic and provide insightful and detailed responses to questions related to the topic. You will choose the topic based on the keywords. I will provide the keywords in a list format, and you will give informative and detail responses, including relevant information, context and any other details that might be necessary. Your responses should demonstrate a deep understanding of the topic and be presented in a teaching and scholarly manner. Please avoid providing general information or summaries. 
+
+"""
+
+DESIGN_PROMPT_2 = f"""
+Provide me detailed and related insight between keywords: 
+"""
