77fb7846df1413150954b941c89faaf1968b9ed9
Caesario Dito <caesariodito2014@gmail.com>
2023-02-06 01:22:47 +0700

    Update keyword_generator.py
    Adding openai api to generate keywords

--- keyword_generator.py
+++ keyword_generator.py
@@ -2,10 +2,11 @@
 import re
 import nltk
 from nltk.stem import WordNetLemmatizer
-
 from collections import Counter
 
-import read_pdf_pypdf as r
+import load_env as e
+import openai
+openai.api_key = e.OPENAI_API_KEY
 
 # nltk.download('stopwords')
 # nltk.download('wordnet')
@@ -30,21 +31,40 @@ def preprocess_text(text):
     
 	return p_text
 
-def get_keywords(text):
+def get_keywords(text, api=False):
   """This function will be used to get the keywords from the preprocessed text
 
   Args:
       text (str): preprocessed text
+      api (bool, optional): If the parameter is set to True, it will generate keywords with api api. Defaults to False.
 
   Returns:
       list: top 5 keywords from the text input
   """
-  text = preprocess_text(text)
-  term_frequencies = Counter(text.split())
-  potential_words = term_frequencies.most_common()[:5]
+  
+  if api:
+    prompt = "Find the most useful terms in this text below. Only print out the terms. \n\n" + text
+    
+    response = openai.Completion.create(
+    model="text-davinci-003",
+    prompt=prompt,
+    temperature=0.3,
+    max_tokens=512,
+    top_p=1,
+    frequency_penalty=0.5,
+    presence_penalty=0
+    )
+    
+    keywords = response['choices'][0]['text']
+    
+  else:
+    text = preprocess_text(text)
+    term_frequencies = Counter(text.split())
+    potential_words = term_frequencies.most_common()[:5]
 
-  keywords = []
-  for word, _ in potential_words:
-    keywords.append(word)
-  # print(keywords)
-  return keywords
+    keywords = []
+    for word, _ in potential_words:
+      keywords.append(word)
+    # print(keywords)
+
+  return str(keywords)
