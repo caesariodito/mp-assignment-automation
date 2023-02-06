330570b7e017fc0eddc89b3806f93ddee143880e
Caesario Dito <caesariodito2014@gmail.com>
2023-02-06 22:34:21 +0700

    Update input.py
    Adding st.cache

--- input.py
+++ input.py
@@ -1,5 +1,6 @@
 import os
 import easyocr
+import streamlit as st
 
 from pypdf import PdfReader
 
@@ -15,7 +16,7 @@ def read_pdf(pdf):
     return text_list
 # HOELIT FINAL EXAM 2022   1. Write an essay of 850-1500 words.  2. You need at least 4 (four) reliable sources. Write all your references in References section. Cite your sources properly.  3. In your essay, you need to discuss a writer (or more)/ a literary work (or more)/ a literary movement that we have discussed throughout this semester. You may discuss the characteristics, the contribution they have made, the values/ criticism they contain, etc. Below are some sample topics for your essay: a. Shakespeare and his contribution to English language and literature, b. Stream of consciousness as a literary breakthrough in twentieth century English literature, c. Social criticism in Charles Dicken’s works.  4. Provide a title and write your essay in paragraphs. Your essay must have the introduction, body (discussion), and the conclusion. 5. Use the provided template.  6. Submit your essay to the LMS no later than 24 December 2022 (early submission is always welcome).  7. If you want to discuss your plan for the essay, feel free to contact me.  8. Plagiarism might result in score reduction or test failure.
 
-
+@st.cache
 def read_image(imgpath, lang=['id', 'en']):
     reader = easyocr.Reader(lang)
     text_list = reader.readtext(imgpath, detail=0, paragraph=True)
