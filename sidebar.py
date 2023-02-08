fb4676c7ab66703fe93b8bdf356662c43d9c80aa
Caesario Dito <caesariodito2014@gmail.com>
2023-02-08 10:41:30 +0700

    Update sidebar.py
    fix bug

--- sidebar.py
+++ sidebar.py
@@ -1,18 +1,20 @@
 import streamlit as st
 
-st.set_page_config(layout="wide", page_title="Assignment Automation")
 
-with st.sidebar:
-    st.markdown("""
-                See my Github Profile!
-                
-                [![GitHub followers](https://img.shields.io/github/followers/caesariodito?style=social)](https://github.com/caesariodito)
-                
-                Star this repo!
-                
-                [![GitHub Repo stars](https://img.shields.io/github/stars/caesariodito/mp-assignment-automation?style=social)](https://github.com/caesariodito/mp-assignment-automation)
-                
-                Connect with me!
-                
-                [![Linkedin](https://img.shields.io/badge/linked-in-369?style=flat-square&logo=linkedin&logoColor=white&color=blue)](https://www.linkedin.com/in/caesariodito)
-                """)
+def sidebar():
+    st.set_page_config(layout="wide", page_title="Assignment Automation")
+
+    with st.sidebar:
+        st.markdown("""
+                    See my Github Profile!
+                    
+                    [![GitHub followers](https://img.shields.io/github/followers/caesariodito?style=social)](https://github.com/caesariodito)
+                    
+                    Star this repo!
+                    
+                    [![GitHub Repo stars](https://img.shields.io/github/stars/caesariodito/mp-assignment-automation?style=social)](https://github.com/caesariodito/mp-assignment-automation)
+                    
+                    Connect with me!
+                    
+                    [![Linkedin](https://img.shields.io/badge/linked-in-369?style=flat-square&logo=linkedin&logoColor=white&color=blue)](https://www.linkedin.com/in/caesariodito)
+                    """)
