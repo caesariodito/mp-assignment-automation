b469f5efb1e2d0ec9489052efb353c8721bc1c4c
Caesario Dito <caesariodito2014@gmail.com>
2023-02-08 11:04:47 +0700

    Update web.py
    - Adding api input
    - Fix button bug

--- web.py
+++ web.py
@@ -17,6 +17,18 @@ Note: **This project is for educational purposes only and the generated output m
 
 st.write('---')
 
+st.markdown('## Input your OpenAI API')
+st.write("""
+         1. Go to https://platform.openai.com/account/api-keys, register if you haven't
+         2. Generate new secret key
+         3. Save and paste it here
+         """)
+st.text('I will not store or even use your API Key.')
+
+api_key = st.text_input('API SECRET KEY: ', type='password')
+
+st.write('----')
+
 col1, col2 = st.columns(2, gap="medium")
 
 with col1:
@@ -68,21 +80,21 @@ with col1:
             text_list = input.read_image(img_val)
             text = ''.join(str(e+'\n') for e in text_list)
 
-    if file != None or genre == 'Text':
-        text_input = st.text_area("Your Question", value=text, height=400)
-
     # disabled the button
     st.session_state.disabled = True
 
-    # Run Process
-    if text_input != '':
-        st.session_state.disabled = False
+    if file != None or genre == 'Text':
+        text_input = st.text_area("Your Question", value=text, height=400)
+        # Run Process
+        if text_input != '' and api_key != '':
+            st.session_state.disabled = False
 
+    st.text("Please fill 'Your Question' and 'API SECRET KEY' to continue.")
     solve_button = st.button(
         'Solve Question!', key='but_solve', disabled=st.session_state.get('disabled'))
 
     if solve_button and text_input != '':
-        res, keywords = solve(text_input)
+        res, keywords = solve(text_input, api_key)
         clicked = True
         st.success('Done!')
 
